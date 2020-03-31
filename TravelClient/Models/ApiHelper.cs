43ead1cf9e26a6b8ef6d2099a4336ad0912aad9a
Benjamin Thom <jaminthom@gmail.com>
2020-03-31 14:03:39 -0700

    update TravelClient/Models/ApiHelper.cs
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

--- TravelClient/Models/ApiHelper.cs
+++ TravelClient/Models/ApiHelper.cs
@@ -5,17 +5,17 @@ namespace TravelClient.Models
 {
   class ApiHelper
   {
-    public static async Task<string> GetAllLocations()
+    public static async Task<string> Get(int id)
     {
       RestClient client = new RestClient("http://localhost:5000/api");
-      RestRequest request = new RestRequest($"locations", Method.GET);
+      RestRequest request = new RestRequest($"locations/{id}", Method.GET);
       var response = await client.ExecuteTestAsync(request);
       return response.Content;
     }
-    public static async Task<string> GetAllExperiences()
+    public static async Task<string> Get(int id)
     {
       RestClient client = new RestClient("http://localhost:5000/api");
-      RestRequest request = new RestRequest($"experiences", Method.GET);
+      RestRequest request = new RestRequest($"experiences/{id}", Method.GET);
       var response = await client.ExecuteTestAsync(request);
       return response.Content;
     }
