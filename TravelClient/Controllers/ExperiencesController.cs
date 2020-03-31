1e0472de2e89b24b5ec309c9b3a011ac1191bc22
Benjamin Thom <jaminthom@gmail.com>
2020-03-31 13:29:54 -0700

    update TravelClient/Controllers/ExperiencesController.cs
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

--- TravelClient/Controllers/ExperiencesController.cs
+++ TravelClient/Controllers/ExperiencesController.cs
@@ -4,3 +4,50 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TravelClient.Models;
+using TravelClient.Models;
+
+namespace CretaceousClient.Controllers
+{
+  public class ExperiencesController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allExperiences = Experience.GetExperiences();
+      return View(allExperiences);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Experience experience)
+    {
+      Experience.Post(experience);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+      var experience = Experience.GetDetails(id);
+      return View(experience);
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var experience = Experience.GetDetails(id);
+      return View(experience);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Experience experience)
+    {
+      experience.ExperienceId = id;
+      Experience.Put(Experience);
+      return RedirectToAction("Details", id);
+    }
+
+    public InictionResult Delete(int id)
+    {
+      Experience.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
+}
