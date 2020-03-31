48b0bcad44dd51c42ea85605442c1865df0b427e
Benjamin Thom <jaminthom@gmail.com>
2020-03-31 14:02:48 -0700

    update TravelClient/Controllers/LocationsController.cs
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

--- TravelClient/Controllers/LocationsController.cs
+++ TravelClient/Controllers/LocationsController.cs
@@ -19,7 +19,33 @@ namespace TravelClient.Controllers
     [HttpPost]
     public IActionResult Index(Location location)
     {
-      Location.Post(location)
+      Location.Post(location);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+      var location = Location.GetDetails(id);
+      return View(location);
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var location = Location.GetDetails(id);
+      return View(location);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Location location)
+    {
+      location.LocationId = id;
+      Location.Put(Location);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Location.Delete(id);
       return RedirectToAction("Index");
     }
   }
