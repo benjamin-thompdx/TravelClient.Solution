27dd25d3f27ebad180314dda1c21b9a69c6f5e10
Benjamin Thom <jaminthom@gmail.com>
2020-03-31 13:32:52 -0700

    update TravelClient/Models/Experience.cs
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

--- TravelClient/Models/Experience.cs
+++ TravelClient/Models/Experience.cs
@@ -2,3 +2,54 @@ using System.Collections.Generic;
 using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
+namespace TravelClient.Models
+{
+  public class Experience
+  {
+    public int ExperienceId { get; set; }
+    public string Author { get; set; }
+    public string Review { get; set; }
+    public int Rating { get; set; }
+    public int LocationId { get; set; }
+
+    public static List<Experience> GetExperiences()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Experience> experienceList = JsonConvert.DeserializeObject<List<Experience>>(jsonResponse.ToString());
+
+      return experienceList;
+    }
+
+    public static Experience GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Experience experience = JsonConvert.DeserializeObject<Experience>(jsonResponse.ToString());
+
+      return experience;
+    }
+
+    public static void Post(Experience experience)
+    {
+      string jsonExperience = JsonConvert.SerializeObject(experience);
+      var apiCallTask = ApiHelper.Post(jsonExperience);
+    }
+
+    public static void Put(Experience experience)
+    {
+      string jsonExperience = JsonConvert.SerializeObject(experience);
+      var apiCallTask = ApiHelper.Put(experience.ExperienceId, jsonExperience);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
+  }
+}
