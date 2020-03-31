d74965a5bf3a45a3b7a69a34392cb5961bd40fad
Benjamin Thom <jaminthom@gmail.com>
2020-03-31 13:30:20 -0700

    update TravelClient/Controllers/LocationsController.cs
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

--- TravelClient/Controllers/LocationsController.cs
+++ TravelClient/Controllers/LocationsController.cs
@@ -4,3 +4,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TravelClient.Models;
+
+namespace TravelClient.Controllers
+{
+  public class LocationsController : Controllers
+  {
+    public IActionResult Index()
+    {
+      var allLocations = Location.GetLocations();
+      return View(allLocations);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Location location)
+    {
+      Location.Post(location)
+      return RedirectToAction("Index");
+    }
+  }
+}
